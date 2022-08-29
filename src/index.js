109a1a24783f32d614d422a6e14f778f3b3be2a8
Dicki Prasetya <semenjakpetang176@gmail.com>
2022-08-29 22:22:16 +0700

    push

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-// 1. import react 
+// 1. import react
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 // 2. get id root
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 
-// 3. pengendalian komponen react 
+// 3. pengendalian komponen react
 const root = ReactDOM.createRoot(el);
 
-// 4. buat komponen
-function App(){
-    var date = new Date();
-    var time = date.toLocaleTimeString();
-    return <h1>{time}</h1>;
+// 4. buat komponen react
+function App() {
+  return <textarea spellCheck />;
 }
 
 // 5. tampilkan komponen
-root.render(<App/>);
+root.render(<App />);
